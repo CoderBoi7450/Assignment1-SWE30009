2d42a4b4340a908bc06a9cf17994b87d7f7def65
Boss Boy <52315766+CoderBoi7450@users.noreply.github.com>
2023-08-18 12:09:33 +1000

    init files

---
+++ Task4.py
@@ -0,0 +1,51 @@
+def testResult(symbolOne, symbolTwo, a, b):
+    firstOperation = OperationOne(symbolOne, a,b)
+    secondOperation = OperationSecond(symbolTwo, firstOperation)
+    return secondOperation
+
+def OperationOne(symbol, a, b):
+    if symbol == "+":
+        return a + b
+    elif symbol == "-":
+        return a - b
+    elif symbol == "*":
+        return a * b
+    elif symbol == "/":
+        return a / b
+    
+def OperationSecond(symbol, a):
+    if symbol == "+":
+        return a + 2
+    elif symbol == "-":
+        return a - 2
+    elif symbol == "*":
+        return a * 2
+    elif symbol == "/":
+        return a / 2
+    
+
+def PossibleValues(rangeValue):
+    b = 1
+    possibleOperator = ["+", "-", "*", "/"]
+    testCaseResult = []
+
+    for i in range (-rangeValue,rangeValue):
+        for symbolOne in possibleOperator:
+            for symbolTwo in possibleOperator:
+                correctAnswer = testResult("-", "*", i, b)
+                if symbolOne == "-" and symbolTwo == "*":
+                    continue
+                result = testResult(symbolOne, symbolTwo, i, b)
+                testCaseResult.append(result)
+
+        if correctAnswer in testCaseResult:
+            print("value of a " + str(i))
+            print("Correct Answer " + str(correctAnswer))
+            print(str(testCaseResult) + "\n")
+        testCaseResult.clear()
+    
+     
+
+
+if __name__ == "__main__":
+    PossibleValues(10000)
